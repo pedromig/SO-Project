4e297efbe499dbd61cf0545d4e760affb038d8b4
pedromig <pedromiguelrodrigues2000@gmail.com>
2019-12-25 15:03:53 +0000

    Adding test commands

---
+++ cmake-build-debug/CMakeFiles/Airport.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/pedro/Documents/Universidade/2ºano/1ºSemestre/SO/Project/SO-Project/ControlTower.c" "/home/pedro/Documents/Universidade/2ºano/1ºSemestre/SO/Project/SO-Project/cmake-build-debug/CMakeFiles/Airport.dir/ControlTower.c.o"
+  "/home/pedro/Documents/Universidade/2ºano/1ºSemestre/SO/Project/SO-Project/SimulationManager.c" "/home/pedro/Documents/Universidade/2ºano/1ºSemestre/SO/Project/SO-Project/cmake-build-debug/CMakeFiles/Airport.dir/SimulationManager.c.o"
+  "/home/pedro/Documents/Universidade/2ºano/1ºSemestre/SO/Project/SO-Project/SimulationUtils.c" "/home/pedro/Documents/Universidade/2ºano/1ºSemestre/SO/Project/SO-Project/cmake-build-debug/CMakeFiles/Airport.dir/SimulationUtils.c.o"
+  "/home/pedro/Documents/Universidade/2ºano/1ºSemestre/SO/Project/SO-Project/logging.c" "/home/pedro/Documents/Universidade/2ºano/1ºSemestre/SO/Project/SO-Project/cmake-build-debug/CMakeFiles/Airport.dir/logging.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/Airport.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "Airport"
+  "Airport.pdb"
+  "CMakeFiles/Airport.dir/ControlTower.c.o"
+  "CMakeFiles/Airport.dir/SimulationManager.c.o"
+  "CMakeFiles/Airport.dir/SimulationUtils.c.o"
+  "CMakeFiles/Airport.dir/logging.c.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Airport.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
